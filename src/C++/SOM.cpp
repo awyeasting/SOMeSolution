2c1c8bd9ee060cd87ae4b2af3ebbcfadb99c29f0
Alec Yeasting <awyeasting@gmail.com>
2020-03-06 18:44:41 -0800

    Implemented SOM saving and loading to and from any stream capable source. Did general code cleanup (commenting, renaming, restructuring).

--- src/C++/SOM.cpp
+++ src/C++/SOM.cpp
@@ -1,66 +1,63 @@
-#include "pch.h"
+#include "SOM.h"
 
-/* SOM Constructor
-Desc: Intitializes a SOM with arguments:
-	width - int width of map
-	height - int height of map
-	hex - bool optional ability to intialize hexagonal map. [HEX OPTION NOT IMPLEMENTED NEED]
-
+/* 
+	Construct untrained SOM with given lattice width and height
 */
-SOM::SOM(int width, int height)
+SOM::SOM(unsigned int width, unsigned int height)
 {
-	_width = width;
-	_height = height;
+	this->_width = width;
+	this->_height = height;
 
 	// Initialize map randomly
-	_weights = new double**[_width];
+	this->_weights = new double**[_width];
 	for (int i = 0; i < _width; i++)
 	{
-		_weights[i] = new double*[height];
+		this->_weights[i] = new double*[height];
 	}
 }
 
 /*
-	The main function for training. takes in the training data with a pointer to an array of doubles.
-	also takes in the arguments of total number of iterations and initial learning rate.
+	Construct SOM from a saved SOM width, height, and set of weights
 */
-void SOM::train_data(double *trainData[], int num_examples, int dimensions, int iterations, double initial_learning_rate)
+SOM::SOM(std::istream &in) {
+	this->load_weights(in);
+}
+
+/*
+	Train the SOM using a set of training data over a given number of epochs with a given learning rate
+*/
+void SOM::train_data(double *trainData[], unsigned int num_examples, unsigned int dimensions, int epochs, double initial_learning_rate)
 {
 	// Randomly initialize weights
-	_dimensions = dimensions;
+	this->_dimensions = dimensions;
 	for (int i = 0; i < _width; i++) {
 		for (int j = 0; j < _height; j++) {
-			double* randWeights = randWeight(_dimensions);
-			_weights[i][j] = randWeights;
+			double* randWeights = SOM::randWeight(_dimensions);
+			this->_weights[i][j] = randWeights;
 		}
 	}
 
-	int iterations_counter = 0;
-	double current_learning = 0.0;
-	double neighborhood_radius = 0.0;
 	double initial_map_radius = _width < _height ? ((double)_width) / 2.0 : ((double)_height) / 2.0;
-
-	double time_constant = double(iterations) / log(initial_map_radius);
+	double time_constant = double(epochs) / log(initial_map_radius);
 
 	normalizeData(trainData, num_examples);
 
-	//Main Training Loop
-	while (iterations_counter < iterations)
-	{
-		current_learning = initial_learning_rate * exp(-double(iterations_counter) / time_constant);
+	// Loop through all epochs
+	double learning_rate, neighborhood_radius;
+	for (int epoch = 0; epoch < epochs; epoch++) {
+		learning_rate = initial_learning_rate * exp(-double(epoch) / time_constant);
+		neighborhood_radius = initial_map_radius * exp(-(double(epoch) / time_constant));
 
-		int train_exam = 0;
-		//For each example in our training set.
-		while (train_exam < num_examples)
-		{
+		// Loop through all training data points
+		for (int example = 0; example < num_examples; example++) {
+			// Find the BMU for the current example
 			int bmu_x, bmu_y;
 			double bmu_dist = DBL_MAX;
-
 			for (int i = 0; i < _width; i++)
 			{
 				for (int j = 0; j < _height; j++)
 				{
-					double temp_dist = EucDist(_weights[i][j], trainData[train_exam]);
+					double temp_dist = EucDist(_weights[i][j], trainData[example]);
 					if (temp_dist < bmu_dist)
 					{
 						bmu_dist = temp_dist;
@@ -70,91 +67,132 @@ void SOM::train_data(double *trainData[], int num_examples, int dimensions, int 
 				}
 			}
 
-			neighborhood_radius = initial_map_radius * exp(-(double(iterations_counter) / time_constant));
-
+			// Update weights for every node
 			for (int i = 0; i < _width; i++)
 			{
 				for (int j = 0; j < _height; j++)
 				{
-					//Loops through every node in the array and calculates the euclidean squared distance away
+					// Calculate the euclidean distance of the current node from the BMU
 					double euclid_away = (i - bmu_x) *
 						(i - bmu_x) +
 						(j - bmu_y) *
 						(j - bmu_y);
 
-
-					double widthSq = neighborhood_radius * neighborhood_radius;
-
-					//Compares the squared euclid distance with the current iteration radius squared. 
-					//If the euclidean dist away is less than neighborhood squared, calculate influence and update
-					//the nodes weights.
-					//if (euclid_away < widthSq)
-					//{
-					double influence = exp(-(euclid_away) / (2 * widthSq));
-					updateNodeWeights(i, j, trainData[train_exam], current_learning, influence);
-					//}
+					// Update the node's weight using gaussian curve
+					double influence = exp(-(euclid_away) / (2 * neighborhood_radius * neighborhood_radius));
+					updateNodeWeights(i, j, trainData[example], learning_rate, influence);
 				}
 			}
-			train_exam++;
 		}
-
-		iterations_counter++;
-
 	}
 }
 
-void SOM::load_weights(std::string path_to_weights_file)
+/*
+	Save the width and height of the SOM followed by the weights for each node with a different node's weights on every line
+*/
+void SOM::save_weights(std::ostream &out)
 {
-
-}
-
-//Saves the weights with the first line of the file being the width seperated by a space and then the height
-//Each subsequent row is a node's weight. Right Now it's hardcoded to save 
-void SOM::save_weights(std::string path_to_weights_file)
-{
-	std::string temp = path_to_weights_file;
-	std::ofstream out;
-	out.open(path_to_weights_file);
-	out << _width << " " << _height << std::endl;
-	for (int i = 0; i < _width; i++)
+	out << this->_width << " " << this->_height << std::endl;
+	for (int i = 0; i < this->_width; i++)
 	{
-		for (int j = 0; j < _height; j++)
+		for (int j = 0; j < this->_height; j++)
 		{
-			for (int k = 0; k < _dimensions; k++) {
+			for (int k = 0; k < this->_dimensions; k++) {
 				if (k != 0) {
 					out << " ";
 				}
-				out << _weights[i][j][k];
+				out << this->_weights[i][j][k];
 			}
 			out << std::endl;
 		}
 	}
-	out.close();
 }
 
 /*
-	Normalizes the data. Right now hardcoded to normalize color data.
-	In future will be changed to normalize to normalize by feature.
+	Load a trained SOM that was saved using the same algorithm as save_weights from an input stream
 */
-void SOM::normalizeData(double **trainData, int num_examples)
+void SOM::load_weights(std::istream &in)
 {
-	for (int i = 0; i < num_examples; i++)
-	{
-		for (int j = 0; j < _dimensions; j++)
-		{
-			trainData[i][j] = trainData[i][j] / 255;
+	// Load SOM dimensions first
+	in >> this->_width >> this->_height;
+
+	// Generate 3d array for storing the node weights
+	this->_weights = new double**[this->_width];
+	for (int i = 0; i < _width; i++) {
+		_weights[i] = new double*[this->_height];
+	}
+
+	// Read first line of matrix to get the dimensionality of weights
+	this->_dimensions = 0;
+	std::string line;
+	std::getline(in, line);
+	std::getline(in, line);
+	std::stringstream ss(line);
+	std::vector<double> line1;
+	double temp;
+	while (ss >> temp) {
+		this->_dimensions++;
+		line1.push_back(temp);
+	}
+
+	// Put first line of matrix into an array in the 3d weights array
+	this->_weights[0][0] = new double[this->_dimensions];
+	for (int k = 0; k < this->_dimensions; k++) {
+		_weights[0][0][this->_dimensions - k - 1] = line1.back();
+		line1.pop_back();
+	}
+
+	// Read the rest of the 3d array in
+	for (int i = 0; i < this->_width; i++) {
+		for (int j = (i == 0 ? 1 : 0); j < this->_height; j++) {
+			this->_weights[i][j] = new double[this->_dimensions];
+			for (int k = 0; k < _dimensions; k++) {
+				in >> this->_weights[i][j][k];
+			}
 		}
 	}
 }
 
-void SOM::updateNodeWeights(int x, int y, double* example, double learning_rate, double influence) {
-	for (int k = 0; k < _dimensions; k++)
+/*
+	Normalizes given data to be between 0 and 1 for each feature
+*/
+void SOM::normalizeData(double **trainData, int num_examples)
+{
+	// Find the max and min value for each feature then use it to normalize the feature
+	this->_featureMaxes = new double[this->_dimensions];
+	this->_featureMins = new double[this->_dimensions];
+	for (int j = 0; j < this->_dimensions; j++)
 	{
-		_weights[x][y][k] += influence * learning_rate * (example[k] - _weights[x][y][k]);
+		this->_featureMaxes[j] = -DBL_MAX;
+		this->_featureMins[j] = DBL_MAX;
+		for (int i = 0; i < num_examples; i++)
+		{
+			if (trainData[i][j] > this->_featureMaxes[j]) {
+				this->_featureMaxes[j] = trainData[i][j];
+			}
+			if (trainData[i][j] < this->_featureMins[j]) {
+				this->_featureMins[j] = trainData[i][j];
+			}
+		}
+		for (int i = 0; i < num_examples; i++) {
+			trainData[i][j] = (trainData[i][j] - this->_featureMins[j])/this->_featureMaxes[j];
+		}
 	}
 }
 
-//Returns a vector of size, numFeatures, with values between 0 and 1.
+/*
+	Update a node's weights to better match a given example
+*/
+void SOM::updateNodeWeights(int x, int y, double* example, double learning_rate, double influence) {
+	for (int k = 0; k < this->_dimensions; k++)
+	{
+		this->_weights[x][y][k] += influence * learning_rate * (example[k] - this->_weights[x][y][k]);
+	}
+}
+
+/*
+	Generate a vector of size numFeatures
+*/
 double* SOM::randWeight(int numFeatures)
 {
 	double* retVector = new double[numFeatures];
@@ -172,7 +210,7 @@ double* SOM::randWeight(int numFeatures)
 */
 double SOM::EucDist(double* v1, double* v2) {
 	double total = 0.0;
-	for (int i = 0; i < _dimensions; i++) {
+	for (int i = 0; i < this->_dimensions; i++) {
 		total += (v1[i] - v2[i])*(v1[i] - v2[i]);
 	}
 	return sqrt(total);
--- src/C++/SOM.h
+++ src/C++/SOM.h
@@ -1,24 +1,36 @@
+#ifndef SOM_H
+#define SOM_H
+
 #include <limits>
+#include <iostream>
+#include <sstream>
+#include <vector>
 
 class SOM
 {
 public:
-	SOM(int width, int height);
+	SOM(unsigned int width, unsigned int height);
+	SOM(std::istream &in);
 
-	void train_data(double *trainData[], int num_examples, int dimensions, int iterations, double initial_learning_rate);
+	void train_data(double *trainData[], unsigned int num_examples, unsigned int dimensions, int epochs, double initial_learning_rate);
 	
-	void load_weights(std::string path_to_weights_file);
-	void save_weights(std::string path_to_weights_file);
+	void save_weights(std::ostream &out);
 
 private:
 
-	int _width;
-	int _height;
-	int _dimensions;
+	unsigned int _width;
+	unsigned int _height;
+	unsigned int _dimensions;
 	double*** _weights;
+	double* _featureMaxes;
+	double* _featureMins;
+
+	void load_weights(std::istream &in);
 
 	void normalizeData(double **trainData, int num_exampless);
 	void updateNodeWeights(int x, int y, double* example, double learning_rate, double influence);
-	double* randWeight(int numFeatures);
+	static double* randWeight(int numFeatures);
 	double EucDist(double* v1, double* v2);
 };
+
+#endif
