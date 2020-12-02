6e5c086f61019ae481a3e6567ee8e82e34e83530
Aaron Hanson <ahanson@harding.edu>
2020-12-01 20:18:03 -0800

    Add files via upload

--- src/C++/include/SOM.h
+++ src/C++/include/SOM.h
@@ -20,12 +20,12 @@ public:
 	SOM(unsigned int width, unsigned int height);
 	SOM(std::istream &in);
 
-	void train_data(std::string fileName, int fileSize, unsigned int current_rank, unsigned int num_procs, unsigned int epochs, unsigned int dimensions, unsigned int rowCount, int rank_seed, unsigned int map_seed);
+	void train_data(std::string fileName, int fileSize, unsigned int current_rank, unsigned int num_procs, unsigned int epochs, unsigned int dimensions, unsigned int rowCount, int rank_seed, unsigned int map_seed, bool flag);
 	void train_one_epoch(double* localMap, double* train_data, double* numerators, double* denominators, int num_examples, double initial_map_radius, int epoch, double time_constant);
 	static double randWeight();
 	void save_weights(std::ostream &out);
 	double* generateRandomTrainingInputs(unsigned int examples, unsigned int dimensions, int seedValue);
-	double* loadTrainingData(std::fstream& in, unsigned int& rows, unsigned int& cols, int read_count, double* featureMaxes, double* featureMins);
+	double* loadTrainingData(std::fstream& in, unsigned int& rows, unsigned int& cols, int read_count, double* featureMaxes, double* featureMins, bool flag);
 	std::fstream& GotoLine(std::fstream& file, unsigned int num);
 	void printDoubles(double *doubleList, unsigned int numDoubles, unsigned int numLines);
 private:
